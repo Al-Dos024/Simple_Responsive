69ebf111d19985e0af9e9ed234b3a2883e241ed4
Ali Dosoqi <106039568+Al-Dos024@users.noreply.github.com>
2024-09-10 19:23:31 +0300

    add devicepreview and responsive text funnction

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
+import 'package:device_preview/device_preview.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:simple_work_responsiveadaptive/widget/home_view.dart';
 
 void main() {
-  runApp(const SimpleWork());
+  DevicePreview(
+      enabled: !kReleaseMode, builder: (context) => const SimpleWork());
 }
 
 class SimpleWork extends StatelessWidget {
@@ -10,6 +13,29 @@ class SimpleWork extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(home: HomeView());
+    return MaterialApp(
+        builder: DevicePreview.appBuilder,
+        locale: DevicePreview.locale(context),
+        home: const HomeView());
+  }
+}
+
+double getResponsiveFontSize(BuildContext context, {required double fontSize}) {
+  double scaleFactor = getScaleFactor(context);
+  double responsiveFontSize = fontSize * scaleFactor;
+
+  double lowerLimit = fontSize * 0.8;
+  double upperLimit = fontSize * 1.2;
+  return responsiveFontSize.clamp(lowerLimit, upperLimit);
+}
+
+double getScaleFactor(BuildContext context) {
+  double width = MediaQuery.sizeOf(context).width;
+  if (width < 600) {
+    return width / 400;
+  } else if (width < 900) {
+    return width / 700;
+  } else {
+    return width / 1000;
   }
 }
