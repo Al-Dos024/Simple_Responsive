7438a30ecab499f8978f0275aade28800ccf2f30
Ali Dosoqi <106039568+Al-Dos024@users.noreply.github.com>
2024-09-10 14:46:49 +0300

    desktop small edit

--- lib/layout/desktop_layout.dart
+++ lib/layout/desktop_layout.dart
@@ -2,8 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:simple_work_responsiveadaptive/layout/tablet_layout.dart';
 import 'package:simple_work_responsiveadaptive/widget/custom_desktop_item.dart';
 import 'package:simple_work_responsiveadaptive/widget/custom_drawer.dart';
-import 'package:simple_work_responsiveadaptive/widget/custom_list.dart';
-import 'package:simple_work_responsiveadaptive/widget/custom_list_view.dart';
 
 class DesktopLayout extends StatelessWidget {
   const DesktopLayout({super.key});
